f86d1193af7f7fcfcbddc0781a5a42d773f7d39f
Toshiya Takeshita <toshi@atbat.jp>
2021-08-12 16:56:34 +0900

    Initial Commit

---
+++ Tourism/TourismApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourismApp.swift
+//  Tourism
+//
+//  Created by としや on 2021/08/12.
+//
+
+import SwiftUI
+
+@main
+struct TourismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
